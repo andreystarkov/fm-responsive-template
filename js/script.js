4c167c1f43db20dbf21d22fc039fa434007f94c1
Andrey Starkov <im@andreystarkov.ru>
2014-10-02 18:54:50 +0600

    nicey nice

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $.fn.waypoint.defaults = {
-      context: '.st-content',
+      context: '.main',
       continuous: true,
       enabled: true,
       horizontal: false,
@@ -74,26 +74,24 @@ var docWidth = document.documentElement.offsetWidth;
                 borderBottom: '4px solid #43405b',
                 boxShadow: '0 5px 12px rgba(0,0,0,0.35)'
             });
-        }, {offset: '10px'});
+        }, {offset: '-10px'});
 
     function waypointsInit(){
-        /*
+ 
 
-            $('.promo').css({scale: 0, perspective: '100px', rotateX: '90deg'});
+          /*  $('.promo').css({scale: 0, perspective: '100px', rotateX: '90deg'});
             $('#estimate').css({y: '-267px'});
             $('.promo').waypoint(function(direction) {
                  $('.promo').transition({scale: 1, perspective: '100px', rotateX: '0deg'}, 500, 'easeInOutCirc');
-                 $('#estimate').transition({y: '0px'}, 500, 'easeInOutCirc'); alert(direction);
+                 $('#estimate').transition({y: '0px'}, 500, 'easeInOutCirc'); 
             }, { offset: '20%', triggerOnce: true }); 
-        */
+       */
 
             $('.section-cars').css({ padding: '0', height: '0px'});
             $('.wp-appear').css({ x: '1800px' });
             $('.car-item').css({x: '1500px', scale: '0'}); 
             $('.box-price').css({x: '2000px', scale: '0.3', rotate: '90deg'});
             $('.box-estimate h2').css({backgroundColor: 'transparent'});
-  
-
 
 
             $('.header-img').click(function(){
@@ -107,7 +105,7 @@ var docWidth = document.documentElement.offsetWidth;
                 $(this).transition({ x: '0'}, 1200, 'easeOutBack');
             }, {offset: '50%', triggerOnce: true});
 
-           /* $('.button-submit').css({scale: 0});
+            $('.button-submit').css({scale: 0});
             $('.button-submit').waypoint(function(direction) {
                 $(this).transition({scale:1});
                 $('.ico', this).transition({scale: '1.5', x: '-50px', rotate: '80deg'}, 800, 'easeOutBack', function(){
@@ -117,17 +115,17 @@ var docWidth = document.documentElement.offsetWidth;
                     });
                     
                 })
-            }, {offset: '5%', triggerOnce: true});
-           */
+            }, {offset: '75%', triggerOnce: true});
+        
             $('#estimate').waypoint(function(direction) {
 
                 $('.section-cars').transition({ height:'auto', x: '0px', scale: '1', padding: '35px 0 5px'}, function(){
                     for(var i = 0; i < 6; i++){
-                        $('#car-'+i).transition({x: '0px', scale: '1'}, 200*i, 'easeOutBack');
+                        $('#car-'+i).transition({x: '0px', scale: '1'}, 300*i, 'easeOutBack');
                     }     
                 });
 
-            }, {offset: '70%', triggerOnce: true});
+            }, {offset: '75%', triggerOnce: true});
 
             $('#pricing').waypoint(function(direction) {
                     for(var i = 0; i < 4; ++i){
@@ -142,6 +140,7 @@ var docWidth = document.documentElement.offsetWidth;
             $('header h1').css({y: '-800px', x: '-600px', rotate:'90deg'});
             $('header p').css({x: '-1450px', rotate: '60deg', scale: 0.5});
             $('.header-wrapper .button').css({y: '300px', opacity: 0});
+
             setTimeout(function() {
               $('header h1').transition({y: '0px', x: '0px', rotate: '-15deg', scale:'0.8'}, 300, 'easeOutExpo', function(){
                     $('header h1').transition({scale:'1.1', rotate: '5deg'}, 300, 'easeInOutCirc', function(){
@@ -165,7 +164,17 @@ var docWidth = document.documentElement.offsetWidth;
               }); 
             }, 400);
 
-
+            $('.button-submit').hover(function(){
+                 $(this).transition({scale:1});
+                $('.ico', this).transition({scale: '1.5', x: '-50px', rotate: '80deg'}, 800, 'easeOutBack', function(){
+                    $(this).transition({scale: '0.8', x: '30px', rotate: '-20deg'}, 900, function(){
+                        $(this).transition({scale: '1', x: '0px', rotate: '0deg'}, 1200, 'easeOutBack'); 
+                     //   $('.box-estimate h2').transition({backgroundColor: '#dc8c2b'});
+                    });
+                    
+                })
+            })
+            waypointsInit();
 /*
     $('.header-img').each(function(){ $(this).transition({x:'1500px'},1); });
     
